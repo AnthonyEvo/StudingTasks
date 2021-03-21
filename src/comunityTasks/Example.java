d932babfc0ce4a61666283dee5b62306a49c93aa
Anthony <Anthony@MCV-1>
2021-03-21 23:12:35 +0300

    Mission completed

--- src/comunityTasks/Example.java
+++ src/comunityTasks/Example.java
@@ -1,10 +1,9 @@
 package comunityTasks;
 
 public class Example {
-	public static void main(String args[]) {
+	public static void main (String args[]) {
 		new Square(0, 0);
 	}
-
 }
 
 class Square {
@@ -12,23 +11,53 @@ class Square {
 
 	Point axis = new Point(0, 0);
 
-	Square(double posX, double posY) {
+	double angle = 14;
+
+	Square(double posX, double posY){
 		v[0] = new Point(0, 0);
 		v[1] = new Point(0, 3);
-		v[2] = new Point(3, 0);
-		v[3] = new Point(3, 3);
-
+		v[2] = new Point(3, 3);
+		v[3] = new Point(3, 0);
 		axis.setGlobalX(posX);
 		axis.setGlobalY(posY);
-
 		changePosition();
+		
 	}
 
+	void move(Point point) {
+		double sideX = -axis.getLocalX() + point.getLocalX();
+		double sideY = -axis.getLocalY() + point.getLocalY();
+		double radius = Math.sqrt(Math.pow(sideX, 2) + Math.pow(sideY, 2));
+		double localAngle = Math.atan(sideY / sideX);
+		
+		if(radius == 0) {
+			point.setGlobalX(axis.getGlobalX());
+			point.setGlobalY(axis.getGlobalY());
+			System.out.println(point.getGlobalX() + " " + point.getGlobalY());
+			return;
+		}		
+		if(sideX >= 0 && sideY >= 0) {
+			point.setGlobalX(axis.getGlobalX() + radius * Math.cos(localAngle + Math.toRadians(angle)));
+			point.setGlobalY(axis.getGlobalY() + radius * Math.sin(localAngle + Math.toRadians(angle)));
+		}
+		else if(sideX < 0 && sideY >= 0) {
+			point.setGlobalX(axis.getGlobalX() - radius * Math.cos(localAngle + Math.toRadians(angle)));
+			point.setGlobalY(axis.getGlobalY() + radius * Math.sin(localAngle + Math.toRadians(angle)));
+		}
+		else if(sideX >= 0 && sideY < 0) {
+			point.setGlobalX(axis.getGlobalX() + radius * Math.cos(localAngle + Math.toRadians(angle)));
+			point.setGlobalY(axis.getGlobalY() - radius * Math.sin(localAngle + Math.toRadians(angle)));
+		} 
+		else if(sideX < 0 && sideY < 0) {
+			point.setGlobalX(axis.getGlobalX() - radius * Math.cos(localAngle + Math.toRadians(angle)));
+			point.setGlobalY(axis.getGlobalY() - radius * Math.sin(localAngle + Math.toRadians(angle)));
+		}		
+		System.out.println(point.getGlobalX() + " " + point.getGlobalY());
+	}
+	
 	void changePosition() {
-		for (int i = 0; i < v.length; i++) {
-			v[i].setGlobalX(axis.getGlobalX() + v[i].getLocalX());
-			v[i].setGlobalY(axis.getGlobalY() + v[i].getLocalY());
-			System.out.println(v[i].getGlobalX() + " " + v[i].getGlobalY());
+		for(int i = 0; i < v.length; i++) {
+			move(v[i]);
 		}
 	}
 }
@@ -36,33 +65,21 @@ class Square {
 class Point {
 
 	double localX, localY, globalX, globalY;
-
+	
 	Point(double localX, double localY) {
 		this.localX = localX;
 		this.localY = localY;
 	}
 
-	public double getGlobalX() {
-		return globalX;
-	}
+	public double getGlobalX() {return globalX;}
 
-	public double getGlobalY() {
-		return globalY;
-	}
+	public double getGlobalY() {return globalY;}
 
-	void setGlobalX(double globalX) {
-		this.globalX = globalX;
-	}
+	void setGlobalX(double globalX) {this.globalX = globalX;}
 
-	void setGlobalY(double globalY) {
-		this.globalY = globalY;
-	}
+	void setGlobalY(double globalY) {this.globalY = globalY;}
 
-	public double getLocalX() {
-		return localX;
-	}
+	public double getLocalX() {return localX;}
 
-	public double getLocalY() {
-		return localY;
-	}
+	public double getLocalY() {return localY;}
 }
