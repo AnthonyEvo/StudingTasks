d8874de0197bad58de8242e2ac4718dd6fd7b204
Anthony <Anthony@MCV-1>
2021-03-23 15:05:31 +0300

    Display added

--- src/comunityTasks/Example.java
+++ src/comunityTasks/Example.java
@@ -1,27 +1,72 @@
 package comunityTasks;
 
+import javax.swing.*;
+import java.awt.*;
+
+class Display extends JPanel{
+	
+	JFrame window;
+	double form[][];
+	int scale = 5;
+	
+	Display(double[][] form) {
+		
+		this.form = form;
+		
+		window = new JFrame("Square");
+		window.setSize(500, 500);
+		window.setResizable(false);
+		
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		window.add(this);
+		window.setVisible(true);
+		
+		this.repaint();
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		paintSquare(g);
+	}
+	
+	private void paintSquare(Graphics g) {
+		g.drawLine((int)((form[0][0] * scale) - 1), (int)(form[0][1] * scale), (int)((form[0][0] * scale) + 1), (int)(form[0][1] * scale));
+		g.drawLine((int)(form[0][0]  * scale), (int)((form[0][1]  * scale) - 1), (int)(form[0][0]  * scale), (int)((form[0][1]  * scale) + 1));
+		
+		for(int i = 2; i < form.length; i++ ) {
+			g.drawLine((int)(form[i - 1][0] * scale), (int)(form[i - 1][1] * scale), (int)(form[i][0] * scale), (int)(form[i][1]* scale));
+		}
+		
+		g.drawLine((int)(form[form.length - 1][0] * scale), (int)(form[form.length - 1][1] * scale), (int)(form[1][0] * scale), (int)(form[1][1] * scale));
+	}
+}
+
 public class Example {
 	public static void main (String args[]) {
-		new Square(0, 0);
+		Square square = new Square(25, 25);
+		new Display(square.sendPoints());
 	}
 }
 
 class Square {
 	Point v[] = new Point[4];
+	double sideLength = 10;
+	Point axis = new Point(15, 15);
 
-	Point axis = new Point(0, 0);
-
-	double angle = 14;
+	double angle = 315;
 
 	Square(double posX, double posY){
 		v[0] = new Point(0, 0);
-		v[1] = new Point(0, 3);
-		v[2] = new Point(3, 3);
-		v[3] = new Point(3, 0);
+		v[1] = new Point(0, sideLength);
+		v[2] = new Point(sideLength, sideLength);
+		v[3] = new Point(sideLength, 0);
+		
 		axis.setGlobalX(posX);
 		axis.setGlobalY(posY);
-		changePosition();
 		
+		changePosition();
 	}
 
 	void move(Point point) {
@@ -35,7 +80,8 @@ class Square {
 			point.setGlobalY(axis.getGlobalY());
 			System.out.println(point.getGlobalX() + " " + point.getGlobalY());
 			return;
-		}		
+		}
+		
 		if(sideX >= 0 && sideY >= 0) {
 			point.setGlobalX(axis.getGlobalX() + radius * Math.cos(localAngle + Math.toRadians(angle)));
 			point.setGlobalY(axis.getGlobalY() + radius * Math.sin(localAngle + Math.toRadians(angle)));
@@ -56,15 +102,30 @@ class Square {
 	}
 	
 	void changePosition() {
+		
 		for(int i = 0; i < v.length; i++) {
 			move(v[i]);
 		}
+	}
+	
+	public double[][] sendPoints() {
+		
+		double temp[][] = new double[v.length + 1][2];
+		temp[0][0] = axis.getGlobalX();
+		temp[0][1] = axis.getGlobalY();
+		
+		for(int i = 1; i < temp.length; i ++) {
+			temp[i][0] = v[i - 1].getGlobalX();
+			temp[i][1] = v[i - 1].getGlobalY();
+		}
+		
+		return temp;
 	}
 }
 
 class Point {
 
-	double localX, localY, globalX, globalY;
+	private double localX, localY, globalX, globalY;
 	
 	Point(double localX, double localY) {
 		this.localX = localX;
@@ -83,3 +144,4 @@ class Point {
 
 	public double getLocalY() {return localY;}
 }
+
